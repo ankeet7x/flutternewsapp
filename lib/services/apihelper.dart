786b7708fd34745212e09646c8a6f0e69305bf36
ankeetkarki <ankeetkarki30@gmail.com>
2020-11-22 15:01:42 +0545

    Change

--- lib/services/apihelper.dart
+++ lib/services/apihelper.dart
@@ -5,23 +5,6 @@ import 'package:http/http.dart' as http;
 
 import 'newsclass.dart';
 
-// class ApiHelper with ChangeNotifier {
-//   ApiHelper() {
-//     this.fetchNews();
-//   }
-//   List<News> _news = [];
-
-//   List<News> get news => [..._news];
-
-//   fetchNews() async {
-//     String url =
-//         'http://newsapi.org/v2/everything?q=bitcoin&from=2020-09-16&sortBy=publishedAt&apiKey=15a3d32b9ee24dd29643f63623df506f';
-//     final response = await http.get(url);
-//     var data = jsonDecode(response.body) as List;
-//     _news = data.map<News>((json) => News.fromJson(json)).toList();
-//   }
-// }
-
 class ApiHelper extends ChangeNotifier {
   ApiHelper() {
     this.fetchNews();
